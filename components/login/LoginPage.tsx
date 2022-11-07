9b92dd853f864f6709c571113ba252a9b5140f36
dikaaayy <andikayudhistira3@gmail.com>
2022-11-07 08:10:12 +0700

    test email provider

--- components/login/LoginPage.tsx
+++ components/login/LoginPage.tsx
@@ -17,7 +17,7 @@ export default function LoginPage({ closeLogin }: any) {
             Logout
           </button>
         ) : (
-          <button className="rounded-full border-[1px] font-medium transition hover:bg-[#bbae86] dark:hover:bg-[#8a8a8a] w-[75%] h-[8%]" onClick={() => signIn("google")}>
+          <button className="rounded-full border-[1px] font-medium transition hover:bg-[#bbae86] dark:hover:bg-[#8a8a8a] w-[75%] h-[8%]" onClick={() => signIn()}>
             Login with google
           </button>
         )}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,12 +4,17 @@ import GithubProvider from "next-auth/providers/github";
 import { prisma } from "../../../lib/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER!,
+      from: process.env.EMAIL_FROM!,
     }),
     // ...add more providers here
   ],
